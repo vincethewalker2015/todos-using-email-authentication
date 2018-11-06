0da77eee85d2b1125f471c473853ce8afbf745a6
vincentbwalker <vincethewalker@gmail.com>
2018-11-06 16:56:34 +0000

    Ipdated git security vulnerabilities

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   helper_method :current_user, :logged_in?
   
   
+  
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
@@ -17,4 +18,9 @@ class ApplicationController < ActionController::Base
       redirect_to root_path
     end
   end
+  
+  
 end
+  
+
+
