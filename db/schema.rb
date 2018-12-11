dad584d8019200e279996de1f853270c468134d7
vincentbwalker <vincethewalker@gmail.com>
2018-12-11 17:18:10 +0000

    Added remember Digest to Users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180621114848) do
+ActiveRecord::Schema.define(version: 20181211171606) do
 
   create_table "todos", force: :cascade do |t|
     t.string  "name"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20180621114848) do
     t.datetime "updated_at",                      null: false
     t.string   "password_digest"
     t.boolean  "admin",           default: false
+    t.string   "remember_digest"
   end
 
 end
