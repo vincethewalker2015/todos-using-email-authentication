61a68f9465ec6ec03fbef977271ac810cc3d0fb4
Vincent <vincethewalker@yahoo.co.uk>
2019-09-08 13:59:14 +0100

    Upgraded to Rails 5.2

--- db/schema.rb
+++ db/schema.rb
@@ -10,24 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181211214837) do
+ActiveRecord::Schema.define(version: 2018_12_11_214837) do
 
   create_table "todos", force: :cascade do |t|
-    t.string  "name"
-    t.text    "description"
+    t.string "name"
+    t.text "description"
     t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.string   "email"
-    t.datetime "created_at",                        null: false
-    t.datetime "updated_at",                        null: false
-    t.string   "password_digest"
-    t.boolean  "admin",             default: false
-    t.string   "remember_digest"
-    t.string   "activation_digest"
-    t.boolean  "activated",         default: false
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "password_digest"
+    t.boolean "admin", default: false
+    t.string "remember_digest"
+    t.string "activation_digest"
+    t.boolean "activated", default: false
     t.datetime "activated_at"
   end
 
