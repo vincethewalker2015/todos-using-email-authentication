541841b5d72b486e5a2eab4051eb1987e93837e7
Vincent <vincethewalker@yahoo.co.uk>
2020-04-08 11:47:54 +0100

    Resolved dublicate in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,12 +21,6 @@ class User < ApplicationRecord
      SecureRandom.urlsafe_base64
   end
   
-  def authenticated?(attribute, token)
-    digest = send("#{attribute}_digest")
-    return false if digest.nil?
-    BCrypt::Password.new(digest).is_password?(token)
-  end
-  
   def remember
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
