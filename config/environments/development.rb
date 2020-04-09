0d206315ce0fcb527edb45b24876013f184ea422
Vincent <vincethewalker@yahoo.co.uk>
2020-04-09 20:10:19 +0100

    Added Log-in/Log-out in Navbar

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,7 +29,7 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :test
-  host = 'todos-vincethewalker.c9users.io' # Don't use this literally; use your local dev host instead
+  host = 'localhost:3000' # Don't use this literally; use your local dev host instead
   # Use this on the cloud IDE.
   config.action_mailer.default_url_options = { host: host, protocol: 'https' }
 
